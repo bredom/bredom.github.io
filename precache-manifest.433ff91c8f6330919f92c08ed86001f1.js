c860c945405647c4d4ac5878d6e00527668b6680
bredom <domm747@gmail.com>
2019-04-11 03:25:45 +0200

    Adding files to deploying on Github Pages

---
+++ precache-manifest.433ff91c8f6330919f92c08ed86001f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  },
+  {
+    "revision": "24c2b096971dfc0702f1",
+    "url": "/react-tutorial/static/js/main.2af00348.chunk.js"
+  },
+  {
+    "revision": "2822e1e6695fbf0ae41b",
+    "url": "/react-tutorial/static/js/2.44b20be8.chunk.js"
+  },
+  {
+    "revision": "24c2b096971dfc0702f1",
+    "url": "/react-tutorial/static/css/main.3b90374e.chunk.css"
+  },
+  {
+    "revision": "81ea81d2cda00f3260d381ce4e6d87d0",
+    "url": "/react-tutorial/index.html"
+  }
+];
